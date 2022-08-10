425154cb9f3f140234b477c48910666c0ea30123
Kenta Kubo <kkk669@users.noreply.github.com>
2022-08-11 03:08:05 +0900

    Bump swift-log to 1.4.3

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-log.git", .upToNextMinor(from: "1.4.2")),
+        .package(url: "https://github.com/apple/swift-log.git", .upToNextMinor(from: "1.4.3")),
     ],
     targets: [
         .target(
