7b0e71ed945085a9d8370fecae406d4247cf796c
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2025-04-06 06:36:19 +0900

    style: swift-format

--- Sources/PlaygroundHandler.swift
+++ Sources/PlaygroundHandler.swift
@@ -16,16 +16,28 @@ import Logging
     #error("Unsupported runtime")
 #endif
 
+/// A log handler for Swift Playground.
 public struct PlaygroundHandler: Sendable {
     private let label: String
     private var prettyMetadata: String?
+    /// Get or set the entire metadata storage as a dictionary.
+    ///
+    /// - note: `LogHandler`s must treat logging metadata as a value type. This means that the change in metadata must
+    ///         only affect this very `LogHandler`.
     public var metadata = Logger.Metadata() {
         didSet {
             self.prettyMetadata = self.prettify(self.metadata)
         }
     }
+    /// Get or set the configured log level.
+    ///
+    /// - note: `LogHandler`s must treat the log level as a value type. This means that the change in metadata must
+    ///         only affect this very `LogHandler`. It is acceptable to provide some form of global log level override
+    ///         that means a change in log level on a particular `LogHandler` might not be reflected in any
+    ///        `LogHandler`.
     public var logLevel = Logger.Level.info
 
+    /// An initializer.
     public init(label: String) {
         self.label = label
     }
@@ -65,6 +77,20 @@ public struct PlaygroundHandler: Sendable {
 }
 
 extension PlaygroundHandler: LogHandler {
+    /// Output a log to stdout.
+    ///
+    /// This method is called when a `LogHandler` must emit a log message. There is no need for the `LogHandler` to
+    /// check if the `level` is above or below the configured `logLevel` as `Logger` already performed this check and
+    /// determined that a message should be logged.
+    ///
+    /// - parameters:
+    ///     - level: The log level the message was logged at.
+    ///     - message: The message to log. To obtain a `String` representation call `message.description`.
+    ///     - metadata: The metadata associated to this log message.
+    ///     - source: The source where the log message originated, for example the logging module.
+    ///     - file: The file the log message was emitted from.
+    ///     - function: The function the log line was emitted from.
+    ///     - line: The line the log message was emitted from.
     public func log(
         level: Logger.Level,
         message: Logger.Message,
@@ -90,6 +116,13 @@ extension PlaygroundHandler: LogHandler {
         )
     }
 
+    /// Add, remove, or change the logging metadata.
+    ///
+    /// - note: `LogHandler`s must treat logging metadata as a value type. This means that the change in metadata must
+    ///         only affect this very `LogHandler`.
+    ///
+    /// - parameters:
+    ///    - metadataKey: The key for the metadata item
     public subscript(
         metadataKey metadataKey: String
     ) -> Logger.Metadata.Value? {
